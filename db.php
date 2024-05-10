6a88c7364af4e8f5e2dd9ca288db282be0892ada
sinsnone <160766329+sinsnone@users.noreply.github.com>
2024-05-11 01:33:04 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,12 @@
 <?php
-  $conn = new mysqli("localhost", "root", "", "animeheaven");
-  if($conn -> connect_error) {
-      die("Connection failed: " . $conn -> connect_error);
-   }
+$host = "lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$username = "z7zk7ye6mh6z6z66";
+$password = "scxrpluon9bsrpjg";
+$database = "iyawhqajpf7r872p";
+$port = 3306;
+
+$conn = new mysqli($host, $username, $password, $database, $port);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
