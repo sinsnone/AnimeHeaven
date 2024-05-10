935cfb025f9e2f90217a6f223319b48f5b06b583
sinsnone <160766329+sinsnone@users.noreply.github.com>
2024-05-11 01:42:16 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,6 @@
 <?php
-$host = "lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-$username = "z7zk7ye6mh6z6z66";
-$password = "scxrpluon9bsrpjg";
-$database = "iyawhqajpf7r872p";
-$port = 3306;
-
-$conn = new mysqli($host, $username, $password, $database, $port);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+  $conn = new mysqli("mysql://z7zk7ye6mh6z6z66:scxrpluon9bsrpjg@lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/iyawhqajpf7r872p");
+  if($conn -> connect_error) {
+      die("Connection failed: " . $conn -> connect_error);
+   }
 ?>
