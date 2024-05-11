2c5d5fae5170e103bf0b68d16cb1591f0c131847
sinsnone <160766329+sinsnone@users.noreply.github.com>
2024-05-11 02:19:05 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,22 @@
 <?php
-$host = "lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"; // استبدله بالمضيف الصحيح
-$username = "z7zk7ye6mh6z6z66"; // استبدله بالاسم الصحيح للمستخدم
-$password = "scxrpluon9bsrpjg"; // استبدله بكلمة المرور الصحيحة
+$host = "i5n14z.stackhero-network.com"; // استبدله بالمضيف الصحيح
+$username = "root"; // استبدله بالاسم الصحيح للمستخدم
+$password = "1ZCxTwjDO4BK77rghOtWTMSfgJWMHbtJ"; // استبدله بكلمة المرور الصحيحة
 $database = "animeheaven"; // استبدله بقاعدة البيانات الصحيحة
 
 // إنشاء اتصال بقاعدة البيانات
 $conn = new mysqli($host, $username, $password, $database);
+
+// خيارات SSL
+$ssl_options = array(
+    "key" => "/path/to/client-key.pem",
+    "cert" => "/path/to/client-cert.pem",
+    "ca" => "/path/to/ca.pem",
+    "capath" => "/path/to"
+);
+
+// تعيين خيارات SSL
+$conn->ssl_set($ssl_options);
 
 // التحقق من الاتصال
 if ($conn->connect_error) {
