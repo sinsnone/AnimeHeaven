342594e0a7343436cd7e86571897fa47a3ee6656
sinsnone <160766329+sinsnone@users.noreply.github.com>
2024-05-11 02:07:20 +0200

    Update db.php

--- db.php
+++ db.php
@@ -4,8 +4,19 @@ $username = "root"; // استبدله بالاسم الصحيح للمستخدم
 $password = "1ZCxTwjDO4BK77rghOtWTMSfgJWMHbtJ"; // استبدله بكلمة المرور الصحيحة
 $database = "animeheaven"; // استبدله بقاعدة البيانات الصحيحة
 
-// إنشاء اتصال بقاعدة البيانات
-$conn = new mysqli($host, $username, $password, $database);
+// خيارات الاتصال الآمن
+$ssl_options = array(
+    "ssl" => array(
+        "key" => "/path/to/client-key.pem",
+        "cert" => "/path/to/client-cert.pem",
+        "ca" => "/path/to/ca.pem",
+        "capath" => "/path/to",
+        "cipher" => "AES256-SHA"
+    )
+);
+
+// إنشاء اتصال بقاعدة البيانات مع استخدام SSL
+$conn = new mysqli($host, $username, $password, $database, null, null, $ssl_options);
 
 // التحقق من الاتصال
 if ($conn->connect_error) {
