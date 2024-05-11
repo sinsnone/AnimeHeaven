7f0f0db69d743e500de94e1e7a35b77c0e652e80
sinsnone <160766329+sinsnone@users.noreply.github.com>
2024-05-11 02:44:35 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,27 +1,6 @@
 <?php
-$host = "i5n14z.stackhero-network.com"; // استبدله بالمضيف الصحيح
-$username = "root"; // استبدله بالاسم الصحيح للمستخدم
-$password = "1ZCxTwjDO4BK77rghOtWTMSfgJWMHbtJ"; // استبدله بكلمة المرور الصحيحة
-$database = "animeheaven"; // استبدله بقاعدة البيانات الصحيحة
-
-// إنشاء اتصال بقاعدة البيانات
-$conn = new mysqli($host, $username, $password, $database);
-
-// خيارات SSL
-$ssl_options = array(
-    "key" => "/path/to/client-key.pem",
-    "cert" => "/path/to/client-cert.pem",
-    "ca" => "/path/to/ca.pem",
-    "capath" => "/path/to"
-);
-
-// تعيين خيارات SSL
-$conn->ssl_set($ssl_options);
-
-// التحقق من الاتصال
-if ($conn->connect_error) {
-    die("فشل الاتصال: " . $conn->connect_error);
-}
-
-echo "تم الاتصال بنجاح!";
+  $conn = new mysqli("lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "z7zk7ye6mh6z6z66", "scxrpluon9bsrpjg", "iyawhqajpf7r872p");
+  if($conn -> connect_error) {
+      die("Connection failed: " . $conn -> connect_error);
+   }
 ?>
