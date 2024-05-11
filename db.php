509415a2cf8da570d51f6aa93f2f9ce01707a46a
sinsnone <160766329+sinsnone@users.noreply.github.com>
2024-05-11 03:00:25 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = new mysqli("lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "z7zk7ye6mh6z6z66", "scxrpluon9bsrpjg", "iyawhqajpf7r872p");
+  $conn = new mysqli("lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com", "z7zk7ye6mh6z6z66", "scxrpluon9bsrpjg", "animeheaven");
   if($conn -> connect_error) {
       die("Connection failed: " . $conn -> connect_error);
    }
